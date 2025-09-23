14b5d9ac02c2b841002dfe6d47ee330025b0fe40
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-09-23 09:06:45 -0500

    Add files via upload

---
+++ dist/plugin.js
@@ -0,0 +1,6 @@
+exports.version = 0.5
+exports.description = "Block browser development tool"
+exports.apiRequired = 1
+exports.repo = "Kiusky/hfs-DevTools-Blocker"
+exports.frontend_js = 'blocker.js'
+exports.preview = ["https://github.com/user-attachments/assets/ac7b7e77-7add-4d6a-9d5a-0ac196dd2d40"]
